a4758550df06a4fa1cc149125962ecd7dd8b31fc
Unknown <justin.paro@gmail.com>
2018-05-27 14:46:23 -0500

    Refactored to Introduce 'game_functions'

--- project/alien_invasion.py
+++ project/alien_invasion.py
@@ -1,7 +1,8 @@
-import sys
 import pygame
+
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -14,13 +15,8 @@ def run_game():
 
     while True:
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
-        screen.fill(settings.bg_color)
-        ship.blitme()
-
-        pygame.display.flip()
+        gf.update_screen(settings, screen, ship)
 
 run_game()
