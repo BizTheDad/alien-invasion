048497c5a7ae454d8ecfab411af017b1cea7c0b4
Unknown <justin.paro@gmail.com>
2018-05-27 14:35:19 -0500

    Created Ship on the Screen

---
+++ project/alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    pygame.init()
+    settings = Settings()
+    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(settings.bg_color)
+        ship.blitme()
+
+        pygame.display.flip()
+
+run_game()
